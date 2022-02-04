db9af0aace8498087d0387a0570542a2f43319e8
svegapatta <sureshvgknair@gmail.com>
2022-02-04 21:42:38 +0530

    Global trend chart implemented

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend,
+} from 'chart.js'
+import { fetchDailyData } from '../../api'
+import styles from './Chart.module.css';
 
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 const Chart = () => {
+
+    const [dailyData, setDailyData] = useState([])
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI()
+    }, [])
+
+    const LineChart = (
+        dailyData.length
+            ? (
+                <Line
+                    data={{
+                        labels: dailyData.map(({ date }) => date),
+                        datasets: [{
+                            data: dailyData.map(({ confirmed }) => confirmed),
+                            label: 'Infected',
+                            borderColor: '#3333ff',
+                            fill: true
+                        }, {
+                            data: dailyData.map(({ deaths }) => deaths),
+                            label: 'Deaths',
+                            borderColor: 'red',
+                            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                            fill: true
+                        }]
+                    }}
+                />) : null
+    )
+
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {LineChart}
+        </div>
     )
 }
 
